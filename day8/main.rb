46771ce4749297dadda91b098c9407235fb0d962
simon brahan <tntpiranha@gmail.com>
2020-12-09 19:29:25 +0000

    Refactor day 8 part 1 into separate function.

--- day8/main.rb
+++ day8/main.rb
@@ -6,23 +6,28 @@ instructions = File.read("input.txt").split("\n").map {
         { :instr => instr_parts[0], :num => instr_parts[1].to_i }
 }
 
-instr_pointer = 0
-visited_instructions = Set.new
-accumulator = 0
+def solve_part1(instructions)
+    instr_pointer = 0
+    visited_instructions = Set.new
+    accumulator = 0
 
-while !visited_instructions.include?(instr_pointer)
-    visited_instructions.add(instr_pointer)
-    instr = instructions[instr_pointer]
+    while !visited_instructions.include?(instr_pointer)
+        visited_instructions.add(instr_pointer)
+        instr = instructions[instr_pointer]
 
-    case instr[:instr]
-    when "acc"
-        accumulator += instr[:num]
-        instr_pointer += 1
-    when "jmp"
-        instr_pointer += instr[:num]
-    when "nop"
-        instr_pointer += 1
+        case instr[:instr]
+        when "acc"
+            accumulator += instr[:num]
+            instr_pointer += 1
+        when "jmp"
+            instr_pointer += instr[:num]
+        when "nop"
+            instr_pointer += 1
+        end
     end
+
+    return accumulator
 end
 
-puts accumulator
+
+puts solve_part1(instructions)
