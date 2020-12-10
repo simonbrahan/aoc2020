a18db2f7ce6d0548733b68e2c6b2a2df567e2812
simon brahan <tntpiranha@gmail.com>
2020-12-10 19:47:53 +0000

    Day 10 refactor part 1 into method

--- day10/main.rb
+++ day10/main.rb
@@ -1,13 +1,17 @@
 input = File.readlines("input.txt").map { |line| line.to_i }.sort
 
-# Add inbuilt device adapter
-input.push(input[-1] + 3)
+def solve_part1(input)
+    # Add inbuilt device adapter
+    input.push(input[-1] + 3)
 
-gaps = Hash.new { |h, k| h[k] = 0 }
-prev = 0
-input.each { |num|
-    gaps[num - prev] += 1
-    prev = num
-}
+    gaps = Hash.new { |h, k| h[k] = 0 }
+    prev = 0
+    input.each { |num|
+        gaps[num - prev] += 1
+        prev = num
+    }
 
-puts gaps[1] * gaps[3]
+    return gaps[1] * gaps[3]
+end
+
+puts solve_part1(input)
