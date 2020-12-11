1beb00e8a475273a0f9bace4a247a2555ed208a3
simon brahan <tntpiranha@gmail.com>
2020-12-11 21:23:54 +0000

    Day 11 part 2 solved

--- day11/main.rb
+++ day11/main.rb
@@ -37,6 +37,53 @@ def process_by_neighbours(input, x, y)
     return input[y][x]
 end
 
+def look_in_direction(input, x, y, xmod, ymod)
+    loop do
+        x = x + xmod
+        y = y + ymod
+
+        if y < 0 || y >= input.length || x < 0 || x >= input[0].length
+            return nil
+        end
+
+        if (input[y][x]) == "."
+            next
+        end
+
+        return input[y][x]
+    end
+end
+
+def num_visible(input, x, y)
+    out = 0
+
+    dirs = [[0, -1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1]]
+
+    for xmod, ymod in dirs
+        if look_in_direction(input, x, y, xmod, ymod) == "#"
+            out += 1
+        end
+    end
+
+    return out
+end
+
+def process_by_visible(input, x, y)
+    if input[y][x] == "."
+        return "."
+    end
+
+    if input[y][x] == "L" && num_visible(input, x, y) == 0
+        return "#"
+    end
+
+    if input[y][x] == "#" && num_visible(input, x, y) >= 5
+        return "L"
+    end
+
+    return input[y][x]
+end
+
 def process_seats(input, seat_func)
     return input.each_with_index.map do |row, y|
         row.each_with_index.map do |seat, x|
@@ -65,5 +112,22 @@ def solve_part1(input)
     return count_occupied(seats)
 end
 
+def solve_part2(input)
+    seats = input
+
+    loop do
+        prev = seats
+
+        seats = process_seats(seats, :process_by_visible)
+
+        if prev == seats
+            break
+        end
+    end
+
+    return count_occupied(seats)
+end
+
 puts solve_part1(input)
 
+puts solve_part2(input)
